83eff9dd6cebb9771b6262a75f3e9d30f082002a
dhruv verma <146999459+dhruvverma2005@users.noreply.github.com>
2023-10-05 00:43:38 +0400

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("clock")
+
+def time():
+    string = strftime("%H:%M:%S %p")
+    lbl.config(text=string)
+    lbl.after(1000, time)
+
+lbl = Label(root, font=("calibri", 40, "bold"), background="purple", foreground="white")
+lbl.pack(anchor="center")  # Corrected parameter
+
+time()  # Added to call the time function initially
+
+mainloop()
